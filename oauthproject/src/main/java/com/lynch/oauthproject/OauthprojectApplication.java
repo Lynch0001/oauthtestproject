f1525dff3d07146a5a03563c102aa95a92f8f932
Tim Lynch <lynch0001@gmail.com>
2022-08-14 14:13:33 -0400

    Initial Commit

---
+++ oauthproject/src/main/java/com/lynch/oauthproject/OauthprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lynch.oauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthprojectApplication.class, args);
+	}
+
+}
